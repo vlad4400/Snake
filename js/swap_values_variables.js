1b128a0246ca15e417d8c6f9f9252447a6010b4b
vlad4400 <sleepvs@gmail.com>
2020-08-09 15:21:47 +0200

    Added program: Swap values variables

--- js/swap_values_variables.js
+++ js/swap_values_variables.js
@@ -1,1 +1,15 @@
 console.log("worked: swap_values_variables");
+console.log("");
+var
+  a = 10,
+  b = 20;
+
+console.log("Initial values: a = " + a + ", b = " + b);
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log("Initial values: a = " + a + ", b = " + b);
+console.log("");
+console.log("no mistakes: swap_values_variables");

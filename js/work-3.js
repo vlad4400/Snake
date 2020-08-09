4aedb91dda9451e5e2ede7d1ea55137efbf95aea
vlad4400 <sleepvs@gmail.com>
2020-08-10 01:24:02 +0200

    added task-3

--- js/work-3.js
+++ js/work-3.js
@@ -1,5 +1,17 @@
 console.log("start: work3");
 console.log("");
 
+var
+  a = -10,
+  b = -20;
+
+if (a >= 0 && b >= 0) {
+  alert(a - b);
+} else if (a < 0 && b < 0) {
+  alert(a * b);
+} else {
+  alert(a + b);
+}
+
 console.log("");
 console.log("end: work3");

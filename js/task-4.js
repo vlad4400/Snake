ad8f7c28837ab0035f62f374ace21bd5de5e3777
vlad4400 <sleepvs@gmail.com>
2020-08-10 01:44:45 +0200

    added: task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,42 @@
 console.log("start: task-4");
 console.log("");
 
+var a = 4;
+
+switch (a) {
+  case 0:
+    document.write("0 ");
+  case 1:
+    document.write("1 ");
+  case 2:
+    document.write("2 ");
+  case 3:
+    document.write("3 ");
+  case 4:
+    document.write("4 ");
+  case 5:
+    document.write("5 ");
+  case 6:
+    document.write("6 ");
+  case 7:
+    document.write("7 ");
+  case 8:
+    document.write("8 ");
+  case 9:
+    document.write("9 ");
+  case 10:
+    document.write("10 ");
+  case 11:
+    document.write("11 ");
+  case 12:
+    document.write("12 ");
+  case 13:
+    document.write("13 ");
+  case 14:
+    document.write("14 ");
+  case 15:
+    document.write("15 ");
+}
+
 console.log("");
 console.log("end: task-4");

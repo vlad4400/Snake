5dfd10541dee6ffc431c75bf62903076fd3bde5b
vlad4400 <sleepvs@gmail.com>
2020-08-09 15:08:03 +0200

    Added program: Conversation Celsius to Fahrenheit

--- js/conversion_celsius-to-fahrenheit.js
+++ js/conversion_celsius-to-fahrenheit.js
@@ -1,1 +1,7 @@
 console.log("worked: conversion_celsius-to-fahrenheit");
+
+var t = prompt("Enter temperature in Celsius: ");
+t = (9 / 5) * t + 32;
+alert("Temperature in Fahrenheit: " + t);
+
+console.log("no mistakes: conversion_celsius-to-fahrenheit")

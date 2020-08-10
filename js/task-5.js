422c318c365fc76c01b606a9ed3b5ea6973316b1
vlad4400 <sleepvs@gmail.com>
2020-08-10 12:17:26 +0200

    added: task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,27 @@
 console.log("start: task-5");
 console.log("");
 
+function mSum(a, b) {
+  return a + b;
+}
+function mSub(a, b) {
+  return a - b;
+}
+function mMul(a, b) {
+  return a * b;
+}
+function mDiv(a, b) {
+  return a / b;
+}
+
+var
+  a = 23,
+  b = -12;
+
+console.log(mSum(a, b));
+console.log(mSub(a, b));
+console.log(mMul(a, b));
+console.log(mDiv(a, b));
+
 console.log("");
 console.log("end: task-5");

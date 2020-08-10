93b144a72640c22455d93ee5b9d8f11ea7f5fe0f
vlad4400 <sleepvs@gmail.com>
2020-08-10 13:00:30 +0200

    added: task-8

--- js/task-8.js
+++ js/task-8.js
@@ -1,5 +1,17 @@
 console.log("start: task-8");
 console.log("");
 
+function power(val, pow) {
+  if (pow == 1) {
+    return val;
+  } else if (pow >= 1) {
+    return val * power(val, pow - 1);
+  } else {
+    return "error";
+  }
+}
+
+console.log(power(2, 8));
+
 console.log("");
 console.log("end: task-8");

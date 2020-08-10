e712b6607a320570c695544166951ee811b99339
vlad4400 <sleepvs@gmail.com>
2020-08-10 12:47:40 +0200

    added: task-7

--- js/task-7.js
+++ js/task-7.js
@@ -1,5 +1,34 @@
 console.log("start: task-7");
 console.log("");
 
+var
+  a = null,
+  b = 0;
+
+console.log("a < b", a < b);
+console.log("a > b", a > b);
+console.log("a == b", a == b);
+console.log("a === b", a === b);
+console.log("a >= b", a >= b);
+console.log("a <= b", a <= b);
+console.log("a != b", a != b);
+console.log("a !== b", a !== b);
+console.log("a && b", a && b);
+console.log("a & b", a & b);
+console.log("a || b", a || b);
+console.log("a | b", a | b);
+console.log("b < a", b < a);
+console.log("b > a", b > a);
+console.log("b == a", b == a);
+console.log("b === a", b === a);
+console.log("b >= a", b >= a);
+console.log("b <= a", b <= a);
+console.log("b != a", b != a);
+console.log("b !== a", b !== a);
+console.log("b && a", b && a);
+console.log("b & a", b & a);
+console.log("b || a", b || a);
+console.log("b | a", b | a);
+
 console.log("");
 console.log("end: task-7");

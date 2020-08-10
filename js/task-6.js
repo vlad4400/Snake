f22da72874e8c44e485080aeeab93e92fe704b44
vlad4400 <sleepvs@gmail.com>
2020-08-10 12:30:22 +0200

    added: task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,28 @@
 console.log("start: task-6");
 console.log("");
 
+function mathOperation(arg1, arg2, operation) {
+  switch (operation) {
+    case "sum":
+      return arg1 + arg2;
+    case "sub":
+      return arg1 - arg2;
+    case "mul":
+      return arg1 * arg2;
+    case "div":
+      return arg1 / arg2;
+    default:
+      return "error";
+  }
+}
+
+a = 23;
+b = -12;
+
+console.log(mathOperation(a, b, "sum"));
+console.log(mathOperation(a, b, "sub"));
+console.log(mathOperation(a, b, "mul"));
+console.log(mathOperation(a, b, "div"));
+
 console.log("");
 console.log("end: task-6");

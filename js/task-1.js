6a531a17bd147ec7ad525e56bed51fa16557de1e
vlad4400 <sleepvs@gmail.com>
2020-08-14 07:16:46 +0200

    created program's structure

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+console.log("start: task-1");
+console.log("");
+
+console.log("");
+console.log("end: task-1");
---
+++ js/task-4.js
@@ -0,0 +1,5 @@
+console.log("start: task-4");
+console.log("");
+
+console.log("");
+console.log("end: task-4");

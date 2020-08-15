d742a4a019e3cd1d7d2f2c1ae248385ff54a10d0
vlad4400 <sleepvs@gmail.com>
2020-08-15 17:50:25 +0200

    update: task-2-3

--- js/task-2-3.js
+++ js/task-2-3.js
@@ -2,46 +2,66 @@ console.log("start: task-2-3");
 console.log("");
 
 class Goods {
-  // list: name, price;
-  static listGoods = {
-    "yogurt": 249, //price 2.49
-    "socks": 499,
-    "milk": 216,
-    "sugar": 199,
-    "soap": 229,
-    "cheese": 1699
+
+  static listGoods = [
+    {nameGood: "yogurt", price: 249}, //real price 2.49
+    {nameGood: "socks", price: 499},
+    {nameGood: "milk", price: 216},
+    {nameGood: "sugar", price: 199},
+    {nameGood: "soap", price: 229},
+    {nameGood: "cheese", price: 1699}
+  ]
+
+  static getPrice(nameGood) {
+    for (var good of this.listGoods) {
+      if (good.nameGood == nameGood) {
+        return good.price;
+      }
+    }
   }
 
-  static getPrice(name) {
-    return this.listGoods[name];
+  static inListGoods(nameGood) {
+    for (var good of this.listGoods) {
+      if (good.nameGood == nameGood)
+        return true;
+    }
+    return false;
   }
 }
 
 class Basket {
+
   goods = [];
-  push(good) {
-    if (good in Goods.listGoods) {
-      this.goods.push(good);
+
+  push(nameGood) {
+    if (Goods.inListGoods(nameGood)) {
+      this.goods.push(nameGood);
       return true;
     }
     return false;
   }
+
   pop(){}
+
   countBasketPrice() {
     var sum = 0;
-    for (var i = 0; i < this.goods.length; i++) {
-      sum += Goods.getPrice(this.goods[i]);
+    for (var good of this.goods) {
+      sum += Goods.getPrice(good);
     }
-    return sum/100;  //249 -> 2.49
+    return sum/100; // 249 -> 2.49 (2.49 - real price)
   }
 }
 
+/*START MAIN PART*/
 var backet = new Basket();
+
 backet.push("milk");
 backet.push("soap");
 backet.push("cheese");
 backet.push("milk");
+
 console.log("Price: " + backet.countBasketPrice() + " PLN");
+/*END MAIN PART*/
 
 console.log("");
 console.log("end: task-2-3");

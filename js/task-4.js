b4c1850bde9b31fbceae1cdaa2428418885faa15
vlad4400 <sleepvs@gmail.com>
2020-08-15 21:43:05 +0200

    small fix: task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 console.log("start: task-4");
 console.log("");
 
-for (var i = 0; i < 10; i++, console.log(i)) {}
+for (var i = -1; i < 9; i++, console.log(i)) {}
 
 console.log("");
 console.log("end: task-4");

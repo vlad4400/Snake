09521fc60a531ee13e0f013aeb5d50a3cce05029
vlad4400 <sleepvs@gmail.com>
2020-08-17 23:42:09 +0200

    done: task-2

--- js/task-2.js
+++ js/task-2.js
@@ -31,7 +31,6 @@ var works = {
     d2: '2 - 2-й ответ\n',
 };
 
-
 var event, ok;
 var answers = [];
 
@@ -47,6 +46,11 @@ do {//Выводим первый вопрос
         ok = isAnswer(works.a0, event);
     }
 } while (!ok);
+if (event == 1) {
+  answers.push(["a00", "a1"]);
+} else if (event == 2) {
+  answers.push(["a00", "a2"]);
+}
 switch (event) {
     case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
         do {
@@ -59,6 +63,11 @@ switch (event) {
                 ok = isAnswer(works.b0, event);
             }
         } while (!ok);
+        if (event == 1) {
+          answers.push(["b00", "b1"]);
+        } else if (event == 2) {
+          answers.push(["b00", "b2"]);
+        }
         switch (event) {
             case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
                 do {
@@ -71,7 +80,11 @@ switch (event) {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
-
+                if (event == 1) {
+                  answers.push(["d00", "d1"]);
+                } else if (event == 2) {
+                  answers.push(["d00", "d2"]);
+                }
                 break;
             case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
                 do {
@@ -84,7 +97,11 @@ switch (event) {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
-
+                if (event == 1) {
+                  answers.push(["d00", "d1"]);
+                } else if (event == 2) {
+                  answers.push(["d00", "d2"]);
+                }
                 break;
             case -1: // Второе действие
                 break;
@@ -103,6 +120,11 @@ switch (event) {
                 ok = isAnswer(works.c0, event);
             }
         } while (!ok);
+        if (event == 1) {
+          answers.push(["c00", "c1"]);
+        } else if (event == 2) {
+          answers.push(["c00", "c2"]);
+        }
         switch (event) {
             case 1: // Второе действие
                 do {
@@ -115,7 +137,11 @@ switch (event) {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
-
+                if (event == 1) {
+                  answers.push(["d00", "d1"]);
+                } else if (event == 2) {
+                  answers.push(["d00", "d2"]);
+                }
                 break;
             case 2: // Второе действие
                 do {
@@ -128,7 +154,11 @@ switch (event) {
                         ok = isAnswer(works.d0, event);
                     }
                 } while (!ok);
-
+                if (event == 1) {
+                  answers.push(["d00", "d1"]);
+                } else if (event == 2) {
+                  answers.push(["d00", "d2"]);
+                }
                 break;
             case -1: // Второе действие
                 break;
@@ -143,6 +173,15 @@ switch (event) {
 }
 alert('Спасибо за игру');
 
+var step = prompt("Теперь вы можете посмотреть свои ходы!\nДля этого введите номер хода");
+while(true) {
+  if (step >= 1 && step <= 3) {
+    alert("В ходе номер '" + step + "' в ситуации:\n" + works[answers[1][0]] + "\nВы выбрали: \n" +  works[answers[1][1 ]]);
+    break;
+  } else {
+    step = prompt("Вы ввели недопустимый символ, или значение.\nВведите повторно номер хода");
+  }
+}
 //------------------------------------------
 function isAnswer(q, event) {
     if (isNaN(event) || !isFinite(event)) {

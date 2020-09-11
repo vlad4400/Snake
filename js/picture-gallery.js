4832626349a6daed5ba47d4bed155214db0ec025
vlad4400 <sleepvs@gmail.com>
2020-09-11 03:39:21 +0200

    updated 'Picture gallery'
    - fix (didn't work message on error);
    - added adaptive&responsive design.

--- js/picture-gallery.js
+++ js/picture-gallery.js
@@ -1,5 +1,7 @@
 console.log("[picture-gallery.js]# start reading script");
 
+let SIZE_GALERY = 4;
+
 window.onload = initiation;
 
 function initiation() {
@@ -10,29 +12,33 @@ function initiation() {
 
 /*create gallery picture*/
 function createGalleryPictures() {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < SIZE_GALERY; i++) {
     let img = document.createElement("img");
     img.alt = "#";
     img.className = "small-img-item";
+    img.id = `small-img-${i}`;
     img.src = `img/small/image-${i}.jpg`;
     img.onclick = openBigImg;
 
-    document.getElementById("small-img").appendChild(img);
+    document.getElementById("small-img-wrapper").appendChild(img);
   }
-}
 
-/*on action click*/
-function openBigImg(event) {
-  let img = document.createElement("img");
-  img.alt = "#";
-  img.src = event.target.src.replace("img/small/image-", "img/big/image-");
-  img.onerror = function() {
-    alert("File not found!");
-  };
+  /*on action click*/
+  function openBigImg(event) {
+    let img = document.createElement("img");
+    img.alt = "#";
+    img.src = event.target.src.replace("img/small/image-", "img/big/image-");
+    img.id = event.target.id.replace("small", "big");
+    img.className = "big-img";
+    img.onerror = function() {
+      alert("File not found!");
+    };
 
-  let bigImg = document.getElementById("big-img");
-  bigImg.innerText = "";
-  bigImg.appendChild(img);
+    let bigImg = document.getElementById("big-img-wrapper");
+    bigImg.innerText = "";
+    bigImg.appendChild(img);
+  }
+
 }
 
 console.log("[picture-gallery.js]# end reading script");
